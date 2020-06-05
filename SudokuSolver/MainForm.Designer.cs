698df607bdbbdcf30187eabb8505558b262a9f5f
Connor Jolley <36887100+ConnorTJolley@users.noreply.github.com>
2020-06-05 11:21:03 +0100

    Uploaded all the files in the Repo
    Uploaded files

---
+++ SudokuSolver/MainForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SudokuSolver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sudokuGrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.solveBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sudokuGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sudokuGrid
+            // 
+            this.sudokuGrid.AllowUserToAddRows = false;
+            this.sudokuGrid.AllowUserToDeleteRows = false;
+            this.sudokuGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sudokuGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.sudokuGrid.Location = new System.Drawing.Point(-41, -19);
+            this.sudokuGrid.Name = "sudokuGrid";
+            this.sudokuGrid.Size = new System.Drawing.Size(593, 436);
+            this.sudokuGrid.TabIndex = 0;
+            this.sudokuGrid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.sudokuGrid_CellEndEdit);
+            this.sudokuGrid.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.sudokuGrid_PreviewKeyDown);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "";
+            this.Column1.MinimumWidth = 61;
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 61;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "";
+            this.Column2.MinimumWidth = 61;
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 61;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "";
+            this.Column3.MinimumWidth = 61;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column3.Width = 61;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "";
+            this.Column4.MinimumWidth = 61;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column4.Width = 61;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column5.HeaderText = "";
+            this.Column5.MinimumWidth = 61;
+            this.Column5.Name = "Column5";
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column5.Width = 61;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column6.HeaderText = "";
+            this.Column6.MinimumWidth = 61;
+            this.Column6.Name = "Column6";
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column6.Width = 61;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column7.HeaderText = "";
+            this.Column7.MinimumWidth = 61;
+            this.Column7.Name = "Column7";
+            this.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column7.Width = 61;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column8.HeaderText = "";
+            this.Column8.MinimumWidth = 61;
+            this.Column8.Name = "Column8";
+            this.Column8.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column8.Width = 61;
+            // 
+            // Column9
+            // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column9.HeaderText = "";
+            this.Column9.MinimumWidth = 61;
+            this.Column9.Name = "Column9";
+            this.Column9.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column9.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column9.Width = 61;
+            // 
+            // solveBtn
+            // 
+            this.solveBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.solveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solveBtn.Location = new System.Drawing.Point(0, 439);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(550, 23);
+            this.solveBtn.TabIndex = 1;
+            this.solveBtn.Text = "Solve";
+            this.solveBtn.UseVisualStyleBackColor = true;
+            this.solveBtn.Click += new System.EventHandler(this.solveBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(0, 416);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(550, 23);
+            this.clearBtn.TabIndex = 2;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 462);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.solveBtn);
+            this.Controls.Add(this.sudokuGrid);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku Solver";
+            ((System.ComponentModel.ISupportInitialize)(this.sudokuGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView sudokuGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button solveBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ SudokuSolver/MainForm.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        private bool _completed;
+        private Task _solveTask;
+
+        public MainForm()
+        {
+            this.InitializeComponent();
+            this.Prep();
+        }
+
+        private void Prep()
+        {
+            this.StyleGrid();
+            this.FillGrid();
+            this.SetBackColours();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this._completed = false;
+        }
+
+        private void SetBackColours()
+        {
+            for (var i = 0; i < this.sudokuGrid.Rows.Count; i++)
+            {
+                for (var x = 0; x < this.sudokuGrid.ColumnCount; x++)
+                {
+                    if (x >= 3 && x < 6 && i < 3)
+                    {
+                        //// Top Middle Cube
+                        this.sudokuGrid.Rows[i].Cells[x].Style.BackColor = Color.DarkGray;
+                    }
+                    else if (x < 3 && i >= 3 && i < 6)
+                    {
+                        //// Middle Left Cube
+                        this.sudokuGrid.Rows[i].Cells[x].Style.BackColor = Color.DarkGray;
+                    }
+                    else if (x >= 6 && i >= 3 && i < 6)
+                    {
+                        //// Middle Right Cube
+                        this.sudokuGrid.Rows[i].Cells[x].Style.BackColor = Color.DarkGray;
+                    }
+                    else if (x >= 3 && x < 6 && i >= 6 && i < 10)
+                    {
+                        //// Bottom Middle Cube
+                        this.sudokuGrid.Rows[i].Cells[x].Style.BackColor = Color.DarkGray;
+                    }
+                }
+            }
+        }
+
+        private void StyleGrid()
+        {
+            this.sudokuGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            this.sudokuGrid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            this.sudokuGrid.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.sudokuGrid.RowsDefaultCellStyle.Font = new Font("Arial", 18, FontStyle.Bold);
+        }
+
+        private void FillGrid()
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                var row = new DataGridViewRow();
+
+                for (var x = 0; x < 9; x++)
+                {
+                    var cell = new DataGridViewTextBoxCell();
+                    row.Cells.Add(cell);
+                }
+
+                this.sudokuGrid.Rows.Add(row);
+                this.sudokuGrid.Rows[i].Height = 46;
+            }
+        }
+
+        public int GetCellValue(DataGridViewCell cell)
+        {
+            return Convert.ToInt32(cell.Value);
+        }
+
+        public void Solve()
+        {
+            if (this._completed)
+            {
+                return;
+            }
+
+            for (var y = 0; y < 9; y++)
+            {
+                for (var x = 0; x < 9; x++)
+                {
+                    if (this.GetCellValue(this.sudokuGrid.Rows[y].Cells[x]) == 0)
+                    {
+                        if (!this.sudokuGrid.Rows[y].Cells[x].ReadOnly)
+                        {
+                            for (var n = 1; n < 10; n++)
+                            {
+                                if (this.ValueIsPossible(y, x, n))
+                                {
+                                    this.SetCellValueOnUi(this.sudokuGrid.Rows[y].Cells[x], n);
+                                    this.Solve();
+                                    if (this._completed)
+                                    {
+                                        return;
+                                    }
+                                    this.SetCellValueOnUi(this.sudokuGrid.Rows[y].Cells[x], null); //// Set to 0 if can't solve
+                                }
+                            }
+                        }
+                        
+
+                        return;
+                    }
+                }
+            }
+
+            this._completed = true;
+            this._solveTask = null;
+        }
+
+        private void SetCellValueOnUi(DataGridViewCell cell, int? value)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => this.SetCellValue(cell, value)));
+                return;
+            }
+
+            this.SetCellValue(cell, value);
+        }
+
+        private void SetCellValue(DataGridViewCell cell, int? value)
+        {
+            this.sudokuGrid.Rows[cell.RowIndex].Cells[cell.ColumnIndex].Value = value;
+        }
+
+        private bool ValueIsPossible(int y, int x, int n)
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                if (this.GetCellValue(this.sudokuGrid.Rows[y].Cells[i]) == n)
+                {
+                    return false;
+                }
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                if (this.GetCellValue(this.sudokuGrid.Rows[i].Cells[x]) == n)
+                {
+                    return false;
+                }
+            }
+
+            var cubeNumber = this.GetColumnCubeNumber(x);
+            var rowCube = this.GetRowCubeNumber(y);
+
+            return this.CheckCube(rowCube, cubeNumber, n);
+        }
+
+        private bool CheckCube(int rowCube, int cubeNumber, int value)
+        {
+            var rows = this.GetRows(rowCube);
+            var cells = this.GetCells(cubeNumber, rows);
+
+            foreach (DataGridViewCell singleCell in cells)
+            {
+                if (this.GetCellValue(singleCell) == value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<DataGridViewCell> GetCells(int cubeNumber, List<DataGridViewRow> rows)
+        {
+            var columns = new List<DataGridViewCell>();
+
+            if (cubeNumber == 1)
+            {
+                rows.ForEach(singleRow =>
+                {
+                    columns.Add(singleRow.Cells[0]);
+                    columns.Add(singleRow.Cells[1]);
+                    columns.Add(singleRow.Cells[2]);
+                });
+            }
+            else if (cubeNumber == 2)
+            {
+                rows.ForEach(singleRow =>
+                {
+                    columns.Add(singleRow.Cells[3]);
+                    columns.Add(singleRow.Cells[4]);
+                    columns.Add(singleRow.Cells[5]);
+                });
+            }
+            else if (cubeNumber == 3)
+            {
+                rows.ForEach(singleRow =>
+                {
+                    columns.Add(singleRow.Cells[6]);
+                    columns.Add(singleRow.Cells[7]);
+                    columns.Add(singleRow.Cells[8]);
+                });
+            }
+
+            return columns;
+        }
+
+        private List<DataGridViewRow> GetRows(int rowCube)
+        {
+            var rows = new List<DataGridViewRow>();
+
+            if (rowCube == 1)
+            {
+                rows.Add(this.sudokuGrid.Rows[0]);
+                rows.Add(this.sudokuGrid.Rows[1]);
+                rows.Add(this.sudokuGrid.Rows[2]);
+            }
+            else if (rowCube == 2)
+            {
+                rows.Add(this.sudokuGrid.Rows[3]);
+                rows.Add(this.sudokuGrid.Rows[4]);
+                rows.Add(this.sudokuGrid.Rows[5]);
+            }
+            else if (rowCube == 3)
+            {
+                rows.Add(this.sudokuGrid.Rows[6]);
+                rows.Add(this.sudokuGrid.Rows[7]);
+                rows.Add(this.sudokuGrid.Rows[8]);
+            }
+
+            return rows;
+        }
+
+        private int GetRowCubeNumber(int y)
+        {
+            //// Check the cube we are in
+            if (y < 3)
+            {
+                //// First Cubes
+                return 1;
+            }
+
+            if (y < 6)
+            {
+                //// Second Cubes
+                return 2;
+            }
+
+            if (y < 9)
+            {
+                //// Last Cubes
+                return 3;
+            }
+
+            return -1;
+        }
+
+        private int GetColumnCubeNumber(int x)
+        {
+            //// Check the cube we are in
+            if (x < 3)
+            {
+                //// First Cubes
+                return 1;
+            }
+
+            if (x < 6)
+            {
+                //// Second Cubes
+                return 2;
+            }
+
+            if (x < 9)
+            {
+                //// Last Cubes
+                return 3;
+            }
+
+            return -1;
+        }
+
+        private void sudokuGrid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var cell = this.sudokuGrid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var cellValue = cell.Value;
+            try
+            {
+                var intValue = Convert.ToInt16(cellValue);
+
+                if (intValue >= 10 || intValue <= -1)
+                {
+                    this.SetCellToEmpty(cell);
+                }
+            }
+            catch (Exception)
+            {
+                this.SetCellToEmpty(cell); //// Set to 0 as it's not an int at the minute
+            }
+        }
+
+        private void SetCellToEmpty(DataGridViewCell cell)
+        {
+            cell.Value = 0;
+            cell.Value = null;
+        }
+
+        private void sudokuGrid_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            var currentCell = this.sudokuGrid.CurrentCell;
+            if (currentCell.RowIndex == -1 || currentCell.ColumnIndex == -1)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.Back || e.KeyCode == Keys.Delete)
+            {
+                this.SetCellToEmpty(currentCell);
+            }
+        }
+
+        private void solveBtn_Click(object sender, EventArgs e)
+        {
+            this._completed = false;
+            this.SetSpecificCellsReadOnly();
+            this._solveTask = new Task(this.Solve);
+            this._solveTask.Start();
+        }
+
+        private void SetSpecificCellsReadOnly()
+        {
+            foreach (DataGridViewRow singleRow in this.sudokuGrid.Rows)
+            {
+                foreach (DataGridViewCell singleCell in singleRow.Cells)
+                {
+                    var cellValue = this.GetCellValue(singleCell);
+
+                    if (cellValue >= 1)
+                    {
+                        singleCell.ReadOnly = true;
+                    }
+                }
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(@"Are you sure you want to Clear the Board?", @"Clear?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                var task = new Task(this.ClearRows);
+                task.Start();
+            }
+        }
+
+        private void ClearRows()
+        {
+            foreach (DataGridViewRow singleRow in this.sudokuGrid.Rows)
+            {
+                foreach (DataGridViewCell singleCell in singleRow.Cells)
+                {
+                    singleCell.ReadOnly = false;
+                    this.SetCellValueOnUi(singleCell, null);
+                }
+            }
+        }
+    }
+}
